51e480fce1c4005ec07a8fc91a2532476a861327
JuDaF-15 <juandda1505@gmail.com>
2023-07-28 13:01:50 -0500

    agregado un p de prueba

--- src/components/Busetas.vue
+++ src/components/Busetas.vue
@@ -12,10 +12,11 @@
       <input type="text" v-model="tipo" placeholder="tipo" style="width: fit-content;">
       <input type="text" v-model="marca" placeholder="marca" style="margin-top: 10px;width: fit-content;">
       <input type="text" v-model="modelo" placeholder="modelo" style="margin-top: 10px;width: fit-content;">
-      <input type="text" v-model="capacidad" placeholder="capacidad" style="margin-top: 10px;width: fit-content;">
+      <input type="number" v-model="capacidad" placeholder="capacidad" style="margin-top: 10px;width: fit-content;">
     </div><br>
 
     <p>{{ c }}</p>
+    <p>jaja</p>
 
     <div>
       <button @click="registrar">Guardar Vehículo</button>
